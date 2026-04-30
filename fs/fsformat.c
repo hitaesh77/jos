65b052d48b8ccc05009ee33aed61e43f9a39893b
Manav Gagvani <mgagvani@outlook.com>
2026-04-30 16:49:58 -0400

    lab5 init

--- fs/fsformat.c
+++ fs/fsformat.c
@@ -26,7 +26,7 @@
 // Typedef the types that inc/mmu.h needs.
 typedef uint32_t physaddr_t;
 typedef uint32_t off_t;
-typedef int bool;
+// typedef int bool;
 
 #include <inc/mmu.h>
 #include <inc/fs.h>
